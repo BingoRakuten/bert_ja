3e88ac190a33a9116ce783f70709e1002eff72a1
root <shun.sugiyama@gmail.com>
2018-11-08 10:35:03 +0900

    update

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ for task in tasks:
            "--vocab_file=/root/work/multilingual_L-12_H-768_A-12/vocab.txt",
            "--bert_config_file=/root/work/multilingual_L-12_H-768_A-12/bert_config.json",
            "--init_checkpoint=/root/work/multilingual_L-12_H-768_A-12/bert_model.ckpt",
-           "--max_seq_length=256",
+           "--max_seq_length=128",
            "--train_batch_size=32",
            "--learning_rate=2e-5",
            "--num_train_epochs=3.0",
