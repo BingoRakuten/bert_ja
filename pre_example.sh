18a83c06c4305d014b7916fbf68554f06ccc3aad
root <shun.sugiyama@jp21.com>
2018-11-09 11:18:34 +0900

    update: add pretraining script

--- pre_example.sh
+++ pre_example.sh
@@ -3,8 +3,8 @@
 export BERT_BASE_DIR=/root/work/bert/jamodel
 
 python create_pretraining_data.py \
-  --input_file=./test.txt \
-  --output_file=/tmp/tf_examples.tfrecord \
+  --input_file=/root/work/data/splitted_1.txt \
+  --output_file=/root/work/tf_examples.tfrecord \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
   --do_lower_case=True \
   --max_seq_length=128 \
@@ -12,3 +12,19 @@ python create_pretraining_data.py \
   --masked_lm_prob=0.15 \
   --random_seed=12345 \
   --dupe_factor=5
+
+
+export BERT_BASE_DIR=/root/work/uncased_L-12_H-768_A-12
+
+python run_pretraining.py \
+       --input_file=/root/work/tf_examples.tfrecord \
+       --output_dir=/root/work/pretraining_output \
+       --do_train=True \
+       --do_eval=True \
+       --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+       --train_batch_size=32 \
+       --max_seq_length=128 \
+       --max_predictions_per_seq=20 \
+       --num_train_steps=20 \
+       --num_warmup_steps=10 \
+       --learning_rate=2e-5
